93386b272eed7efda0d702a085cef653936acabd
Xatoche <saumonardent@gmail.com>
2025-02-10 15:23:00 +0100

    Connect for game

--- app.js/scrypt.js
+++ app.js/scrypt.js
@@ -1,31 +1,16 @@
-// Sélectionner toutes les cellules du plateau
-var cellules = document.querySelectorAll(".oval");
+const $ovals = document.querySelectorAll(".oval") // [Element] -> boucler dessus pour sélectionner chacun des éléments
 
-// Variable pour suivre le joueur actuel
-var joueurActuel = "jaune";
 
-// Parcourir chaque cellule et ajouter un écouteur d'événement
-cellules.forEach(function (cellule) {
-    cellule.addEventListener("click", function () {
-        var image = cellule.querySelector("./");
 
-        // Vérifier si la cellule est vide ou déjà remplie
-        if (image) {
-            return; // Empêche de changer une case déjà remplie
-        }
 
-        // Créer une nouvelle image pour le jeton
-        var jeton = document.createElement("img");
 
-        if (joueurActuel === "jaune") {
-            jeton.src = "./data/svg/Oval Jaune.svg";
-            joueurActuel = "rouge"; // Changer de joueur
-        } else {
-            jeton.src = "./data/svg/Oval Rouge.svg";
-            joueurActuel = "jaune"; // Changer de joueur
-        }
-
-        // Ajouter le jeton à la cellule
-        cellule.appendChild(jeton);
+$ovals.forEach(function($gameScreenGridCell) {
+    $gameScreenGridCell.addEventListener("click", function () {
+      const dataX = $gameScreenGridCell.getAttribute("data-x");
+      const dataY = $gameScreenGridCell.getAttribute("data-y");
+  
+      console.log(dataX)
+      console.log(dataY)
     });
-});
+})
+
